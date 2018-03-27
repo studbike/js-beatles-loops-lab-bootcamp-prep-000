f9be3ca8f5e49e0749b634993c934dd448310699
grant diamond <grant.a.diamond@gmail.com>
2018-03-27 20:08:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function theBeatlesPlay(musicians, instruments){
     arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }return arr
 }
+
+const facts = [
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
+];
+function johnLennonFacts(arr){
+var i = 0
+while (i<arr.length){
+  arr[i] += "!!!"
+  i++ 
+}
+}
